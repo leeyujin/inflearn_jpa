f93de08ab44b175b71bbfb43f5777f6697954c82
yujinlee <now8567@hanmail.net>
2022-01-24 11:40:41 +0900

    join

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -10,10 +10,16 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    private MemberType type;
 
     public Long getId() {
         return id;
--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -15,22 +15,26 @@ public class jpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            for(int i = 0 ; i< 100 ; i++) {
-                Member member = new Member();
-                member.setUsername("member"+i);
-                member.setAge(i);
-                em.persist(member);
-            }
 
-            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("TeamA");
+            member.setAge(10);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m, Team t where m.username=t.name";
+
+            List<Member> resultList = em.createQuery(query, Member.class)
                     .getResultList();
 
-            for (Member member1 : resultList){
-                System.out.println("member1 = " + member1);
-            }
-
+            System.out.println(resultList.size());
 
             tx.commit();
         } catch (Exception e) {

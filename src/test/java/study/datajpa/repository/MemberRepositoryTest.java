11f0f480404aaa9a8f1952c1edb58a4477d8cfb8
yujinlee <now8567@hanmail.net>
2022-02-14 17:04:31 +0900

    쿼리메소드 기능 - JPA NamedQuery

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -87,4 +87,17 @@ class MemberRepositoryTest {
         List<Member> list = memberRepository.findTop3By();
     }
 
+    @Test
+    public void testNamedQuery(){
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("AAA",20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+    }
+
 }
